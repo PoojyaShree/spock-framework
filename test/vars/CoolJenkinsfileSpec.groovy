f1041d2034708953d819659670ce4104df74d5b7
PoojyaShree <87475087+PoojyaShree@users.noreply.github.com>
2022-01-28 16:04:55 +0530

    Delete test/vars directory

--- test/vars/CoolJenkinsfileSpec.groovy
+++
@@ -1,9 +0,0 @@
-def "Jenkinsfile"() {
-	setup:
-		def Jenkinsfile = loadPipelineScriptForTest("vars/CoolJenkinsfile.groovy")
-	when:
-		Jenkinsfile.run()
-	then:
-		1 * getPipelineMock("node")("legacy", _)
-		1 * getPipelineMock("echo")("hello world")
-}
